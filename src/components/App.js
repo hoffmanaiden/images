953cd3a3947d7b0cfed09f2507be084506f19c44
hoffmanaiden <hoffman.aiden@gmail.com>
2020-07-07 16:48:54 -0400

    removed page count from screen

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import unsplash from "../api/unsplash";
 import SearchBar from './SearchBar';
 import ImageList from "./ImageList";
-import Pages from './Pages';
+// import Pages from './Pages';
 
 
 class App extends Component {
   state = { images: [], pages: 0 };
+  // # of pages on this.state.pages
 
   onSearchSubmit = async (term) => {
     const response = await unsplash.get('/search/photos?page=3', {
@@ -22,7 +23,6 @@ class App extends Component {
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSearchSubmit={this.onSearchSubmit} />
         Found: {this.state.images.length} images
-        <Pages pages={this.state.pages} />
         <ImageList images={this.state.images} />
       </div>
     )
